7257b434ddaa32ff7a4508af530ef56aab51985d
Andrii STADNIK <astadnik@student.unit.ua>
2018-01-14 17:26:11 +0200

    Big improvements

--- test.go
+++ test.go
@@ -1,13 +1,110 @@
 package main
 
-import "net"
-import "fmt"
-import "bufio"
-import "strings" // only needed below for sample processing
+import (
+	"net"
+	"fmt"
+	"bufio"
+	"strings" // only needed below for sample processing
+)
 
 type user struct {
-	nick, pass string
+	nick, pass, name string
 	chans []string
+	mes chan string
+	act chan string
+}
+
+var (
+	users = make(map[string]user)
+	ln net.Listener
+)
+
+func cons() {
+	for {
+		con, _ := ln.Accept()
+		// accept connection on port
+		//fmt.Println("New connection)")
+		go login(con)
+	}
+
+}
+
+func ch_nick(st string) *user {
+	for _, us := range users {
+		if st == us.nick {
+			return &us
+		}
+	}
+	return nil
+}
+
+func ch_user(u string) bool {
+	_, inc := users[u]
+	return inc
+}
+
+func login(con net.Conn) {
+	var n, p, u, nm string
+	sc := bufio.NewScanner(con)
+	for sc.Scan() {
+		st := strings.Split(sc.Text(), " ")
+		switch strings.ToLower(st[0]) {
+		case "nick":
+			if len(st) == 2 && ch_nick(st[1]) == nil {
+				n = st[1]
+				fmt.Println("It's nick :", n)
+			} else {
+				fmt.Println("Wrong nick D:")
+			}
+		case "user":
+			if len(st) > 4 && st[4][0] == ":"[0] && !ch_user(st[1]) {
+				u = st[1]
+				nm = strings.Join(st[4:], " ")
+				fmt.Println("It's user:", u)
+			} else {
+				fmt.Println("Wrong user D:")
+			}
+			default : fmt.Println("Nor user or nick :(  : ", st)
+		}
+		if len(n) > 0 && len(u) > 0 {
+			break
+		}
+	}
+	if len(n) > 0 && len(u) > 0 {
+		users[u] = user{nick: n, pass: p, name: nm, act: make(chan string, 10), mes: make(chan string, 10)}
+		con.Write([]byte(fmt.Sprintln(":server 001 ", u, ": Hehey you're welcome")))
+		go hand(u, sc, con)
+	}
+}
+
+func inp(u string, sc *bufio.Scanner) {
+	for sc.Scan() {
+		txt := sc.Text()
+		if us := ch_nick(strings.Split(txt, " ")[0]); us != nil {
+			us.mes <- strings.Join(strings.Split(txt, " ")[1:], " ") + "\n"
+		} else {
+			fmt.Println(users[u].nick, "wrote:", sc.Text())
+		}
+	}
+	users[u].act <- "disconnected"
+}
+
+func hand(u string, sc *bufio.Scanner, con net.Conn) {
+	go inp(u, sc)
+	f:
+	for {
+		select {
+		case msg := <- users[u].mes:
+			con.Write([]byte(msg))
+		case act := <- users[u].act:
+			switch act {
+			case "disconnected":
+				break f
+			}
+		}
+	}
+	con.Close()
+	fmt.Println(u, " disconnected MWAHAHA")
 }
 
 func main() {
@@ -15,39 +112,9 @@ func main() {
 	fmt.Println("Launching server...")
 
 	// listen on all interfaces
-	ln, _ := net.Listen("tcp", ":8081")
+	ln, _ = net.Listen("tcp", ":8081")
 
-	// accept connection on port
-	con, _ := ln.Accept()
-	sc := bufio.NewScanner(con)
-	fmt.Println("New connection)")
-	users := make(map[string]user)
-	var n, p, u string
-	for sc.Scan() {
-		st := strings.Split(sc.Text(), " ")
-		switch strings.ToLower(st[0]) {
-			case "name":
-				n = strings.Join(st[1:], " ")
-				fmt.Println("It's nick : ", n)
-			case "user":
-				u = strings.Join(st[1:], " ")
-				fmt.Println("It's user: ", u)
-			default : fmt.Println("Nor user or name :(  : ", st)
-		}
-		fmt.Println(len(n), len(u))
-		if len(n) > 0 && len(u) > 0 {
-			users[u] = user{nick: n, pass: p}
-			fmt.Println("potato")
-			break
-		}
+	go cons()
+	for {
 	}
-	for u, _ := range users {
-		fmt.Println(":server 001 ", u, ": Hehey you're welcome")
-	}
-	// output message received
-	//fmt.Print("Message Received:", string(message))
-	// sample process for string received
-	//newmessage := strings.ToUpper(message)
-	// send new string back to client
-	//conn[0].Write([]byte(newmessage + "\n"))
 }
